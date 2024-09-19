a2835a30f8dcd3de64ce17ec8ee868e3a5e05375
e550192 <raphael_mathieu.schreiber@sbb.ch>
2024-09-19 15:04:03 +0200

    niiiiicccccoooooo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 echo "$CLOUDFLARE_ORIGIN_CERTIFICATE" > /etc/ssl/raphael-schreiber.pem
 echo "$CLOUDFLARE_ORIGIN_CA_KEY" > /etc/ssl/raphael-schreiber.key
 
-npx ng serve --host 127.0.0.1 --port 3000 &
+npm run prod &
 
 nginx -t
 nginx -g 'daemon off;'
