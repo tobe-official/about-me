fd35916cf9e562caccca3203c8661dccf69e0ebb
e550192 <raphael_mathieu.schreiber@sbb.ch>
2024-09-23 15:06:54 +0200

    WE STILL THE BEST

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
+#!/bin/sh
 set -e
 echo "$CLOUDFLARE_ORIGIN_CERTIFICATE" > /etc/ssl/raphael-schreiber.pem
 echo "$CLOUDFLARE_ORIGIN_CA_KEY" > /etc/ssl/raphael-schreiber.key
 
+# Remove this line as ng serve is for development
+# npm run prod &
+
+# Ensure Nginx configuration is valid
 nginx -t
 
+# Start Nginx in the foreground
 nginx -g 'daemon off;'
