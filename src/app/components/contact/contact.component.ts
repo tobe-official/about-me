4a84cf121cab1b200a7d53fd147bcf5bafb2f942
e550192 <raphael_mathieu.schreiber@sbb.ch>
2024-10-19 15:09:39 +0200

    feat: added contact and made it responsiv. the design is mostly finished. still need to add i18n. fix: fixed mobile version of about me with the picture and the infoDiv

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {BackgroundComponent} from "../global/background/background.component";
 import {FooterComponent} from "../global/footer/footer.component";
 import {HeaderComponent} from "../global/header/header.component";
 import {TranslateModule} from "@ngx-translate/core";
-import {FormsModule} from "@angular/forms";
-import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import emailjs, {EmailJSResponseStatus} from 'emailjs-com';
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
 
 
 @Component({
@@ -15,21 +18,38 @@ import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
     FooterComponent,
     HeaderComponent,
     TranslateModule,
-    FormsModule
+    FormsModule,
+    MatFormField,
+    ReactiveFormsModule,
+    MatInput,
+    MatLabel,
+    MatButton
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  nameOfCustomer = new FormControl("", [Validators.required]);
+  emailOfCustomer = new FormControl("", [
+    Validators.required,
+    Validators.email,
+    Validators.pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+  ]);
+  textOfCustomer = new FormControl("", [Validators.required]);
+
   public sendEmail(e: Event) {
-    e.preventDefault(); // Verhindert die Standard-Formularaktion
-    emailjs.sendForm('service_nt705ry', 'template_kp1qstl', e.target as HTMLFormElement, 'z_tcBe0UR1PXaeCD1')
-      .then((result: EmailJSResponseStatus) => {
-        console.log('Email sent successfully:', result.text);
-        alert('Email sent successfully');
-      }, (error) => {
-        console.error('Error sending email:', error.text);
-        alert('Failed to send email');
-      });
+    if (this.nameOfCustomer.valid && this.emailOfCustomer.valid && this.textOfCustomer.valid && this.nameOfCustomer.value != "" && this.emailOfCustomer.value != "" && this.emailOfCustomer.value != "") {
+      e.preventDefault(); // Verhindert die Standard-Formularaktion
+      emailjs.sendForm('service_nt705ry', 'template_kp1qstl', e.target as HTMLFormElement, 'z_tcBe0UR1PXaeCD1')
+        .then((result: EmailJSResponseStatus) => {
+          console.log('Email sent successfully:', result.text);
+          alert('Email sent successfully');
+        }, (error) => {
+          console.error('Error sending email:', error.text);
+          alert('Failed to send email');
+        });
+    } else {
+      alert("Please fill all the inputs correctly")
+    }
   }
 }
